c5be2f5b255ad7b36d829730078ec8fcb8416860
John Stack <johnfstack@gmail.com>
2019-08-19 08:02:08 +0100

    Update co6.js

--- docs/co6.js
+++ docs/co6.js
@@ -15,7 +15,10 @@ request.addEventListener('load', function (e) {
     document.getElementById('fifthtags').innerHTML = '5. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[4].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[4].details.confidence + '%';
     document.getElementById('sixthtag').innerHTML = '6. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[5].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[5].details.confidence + '%';
     document.getElementById('seventhtag').innerHTML = '7. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[6].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[6].details.confidence + '%';
-      document.getElementById('eighthtag').innerHTML = '8. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[7].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[7].details.confidence + '%';  
+    document.getElementById('eighthtag').innerHTML = '8. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[7].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[7].details.confidence + '%'; 
+    document.getElementById('eigthtag').innerHTML = '10. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[8].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[8].details.confidence + '%';  
+    document.getElementById('tenthtag').innerHTML = '10. ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[9].value + '? ' + el.attributes.multimedia[0].enhancement.rekognition[0].labels[9].details.confidence + '%';  
+
   });  
 });
 
